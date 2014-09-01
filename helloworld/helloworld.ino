2f1feabb0770362bf94c4cf54b1a611d6c17fa38
Xu, Martin <mxu2008@gmail.com>
2014-08-31 17:20:17 -0700

    added more code

---
+++ helloworld/helloworld.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+
+  delay(1000);               // wait for a second
+  Serial.println("hello world");
+}
