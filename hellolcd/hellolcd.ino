eb04809a7549f8dcc4c1f04401140e660e4d3890
Xu, Martin <mxu2008@gmail.com>
2014-08-31 17:34:18 -0700

    added hello lcd codes

---
+++ hellolcd/hellolcd.ino
@@ -0,0 +1,15 @@
+#include <Wire.h>
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd( 8, 9, 4, 5, 6, 7 );
+
+void setup()
+{
+  lcd.begin(16, 2);
+  lcd.print("hello lcd!");
+}
+
+void loop()
+{
+    // your main loop code here...
+}
